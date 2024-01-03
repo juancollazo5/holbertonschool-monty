cde2a329dccc07bbf93c2cf8709e8bc1abfad3ba
CreatorCosmo <girizarrymartell14@gmail.com>
2024-01-03 15:52:46 -0400

    Added to the program monty.c a condition to print an error message when it cannot open the file

--- monty.c
+++ monty.c
@@ -10,6 +10,14 @@ int main(int argc, char *argv[])
 		fprintf(stderr, "USAGE: monty file\n"); /* prints error message then exits */
 		exit(EXIT_FAILURE);
 		}
+	/* Attempt to open the file */
+	FILE *file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+	exit(EXIT_FAILURE);
+	}
 
 return 0;
 }
