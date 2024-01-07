a69b03c1055bcdedfbe1e6ab3bb68367a34358bb
juancollazo5 <juancollazo577@outlook.com>
2024-01-07 07:50:24 +0000

    Function Update
    Conditional blocks were consolidated to make the code clearer and more concise and the file handling was simplified by removing the separate declaration and assignment of file and combining it with the file open check, plus other minor changes

--- monty.c
+++ monty.c
@@ -1,47 +1,42 @@
 #include "monty.h"
 
 /**
-	* main - Main function for monty interpreter
-	* @argc: Number of command line arguments
-	* @argv: Name of the file to open
-	* Return: 0
-*/
-
-
+ * main - Main function for monty interpreter
+ * @argc: Number of command line arguments
+ * @argv: Name of the file to open
+ * Return: 0
+ */
 int main(int argc, char *argv[])
 {
+	char *line = NULL;
+	size_t len = 0;
+	stack_t *stack = NULL;
 
-char *line = NULL;
-size_t len = 0;
-stack_t *stack = NULL;
-
-/* checks the command line arguments to ensure only one file is provided. */
-
-/* checks if number of arguments is 2 (program name and file name)*/
-if (argc != 2)
-{
-fprintf(stderr, "USAGE: monty file\n"); /* prints error message then exits */
-exit(EXIT_FAILURE);
-}
-
-/* Attempt to open the file */
-FILE *file = fopen(argv[1], "r");
-
-if (file == NULL) /* if the file trying to open is NULL */
+	/* checks the command line arguments to ensure only one file is provided. */
+	/* checks if number of arguments is 2 (program name and file name)*/
+	if (argc != 2)
 	{
-	/* print error message and exit */
-	fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-	exit(EXIT_FAILURE);
+		fprintf(stderr, "USAGE: monty file\n"); /* prints error message then exits */
+		exit(EXIT_FAILURE);
+	}
+
+	/* Attempt to open the file */
+	FILE *file = fopen(argv[1], "r");
+
+	if (file == NULL) /* if the file trying to open is NULL */
+	{
+		/* print error message and exit */
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
 	}
 
 	char *line = NULL;
 	size_t len = 0;
 	stack_t *stack = NULL;
 
-
 	while (getline(&line, &len, file) != -1) /* Reads the file line by line */
 	{
-	readprocess_line(line, &stack); /* Process each line provided */
+		readprocess_line(line, &stack); /* Process each line provided */
 	}
 
 	free(line);
@@ -49,4 +44,3 @@ if (file == NULL) /* if the file trying to open is NULL */
 	fclose(file);
 	return (0);
 }
-
