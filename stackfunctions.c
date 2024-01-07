d61660fc9772f0bf0d3d2de479ff25a2dfcb80d2
juancollazo5 <juancollazo577@outlook.com>
2024-01-07 07:58:31 +0000

    Function Update
    I  introduces a new function named pall, which prints all values on the stack and make other minor changes

--- stackfunctions.c
+++ stackfunctions.c
@@ -1,17 +1,35 @@
 #include "monty.h"
 
-/* Function to push a value to the stack data structure */
-        void push(stack_t **stack, int n)
-        {
-        stack_t *new_node = malloc(sizeof(stack_t)); /* Allocate memory for the new node */
+/**
+ * push - Function to push a value to the stack data structure
+ * @stack: Pointer to the top of the stack
+ * @n: Value to be pushed onto the stack
+ */
+void push(stack_t **stack, int n)
+{
+	/* Allocate memory for the new node */
+	stack_t *new_node = malloc(sizeof(stack_t));
 
-        if (new_node == NULL)
-        {
-        fprintf(stderr, "Error: malloc failed\n"); 
-        exit(EXIT_FAILURE);
-        }
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
 
 	new_node->n = n;
 	new_node->next = *stack;
 	*stack = new_node;
+}
+
+/**
+ * pall - Function to print all values on the stack
+ * @stack: Pointer to the top of the stack
+ */
+void pall(stack_t *stack)
+{
+	while (stack != NULL)
+	{
+		printf("%d\n", stack->n);
+		stack = stack->next;
 	}
+}
