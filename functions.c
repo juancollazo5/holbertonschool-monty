abb1c6191e4385ac139de220e6ce1fdb56b5bddb
juancollazo5 <juancollazo577@outlook.com>
2024-01-07 21:31:41 +0000

    function update
     Implement the push and pall opcode in functions.c, Increment line number in readprocess_line

--- functions.c
+++ functions.c
@@ -31,18 +31,25 @@ void readprocess_line(char *line, stack_t **stack)
 		char *arg = strtok(NULL, " \t\n");
 
 		/* Verify if argument is valid */
-		if (arg == NULL || !isdigit(*arg))
+		n = atoi(arg);
+		if (n == 0 && arg[0] != '0')
 		{
 			fprintf(stderr, "L%d: usage: push integer\n", line_number);
 			exit(EXIT_FAILURE);
 		}
-
-		n = atoi(arg);
 		push(stack, n);
 	}
+
 	else if (strcmp(opcode, "pall") == 0)
 	{
 		pall(*stack);
 	}
-	/* other opcodes */
+
+	else
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+		exit(EXIT_FAILURE);
+	}
+
+	line_number++;
 }
