1baccc17bc82640d3090cc0a95699169006b9aa2
juancollazo5 <juancollazo577@outlook.com>
2024-01-08 20:33:23 +0000

    new file
    create new functions file to add pint function

--- functions.c
+++ functions.c
@@ -1,45 +1,21 @@
 #include "monty.h"
 
 /**
-* rpl - read and process lines from the file
-* @content: line to process
-* @stack: pointer to the stack
-* @counter: line number
-* @file: pointer to the input file
-* Return: 0
-*/
-int rpl(char *content, stack_t **stack, unsigned int counter, FILE *file)
+ * pint - print top node on stack
+ * @head: pointer to pointer of first node
+ * @counter: line counter
+ */
+
+void pint(stack_t **head, unsigned int counter)
 {
-	instruction_t opst[] = {
-				{"push", push},
-				{"pall", pall}
-				};
-	unsigned int i = 0;
-	char *op;
-
-	op = strtok(content, " \n\t"); /* Tokenize the content to extract the operation */
-	if (op && op[0] == '#')
-		return (0); /* Ignore comments (lines starting with '#') */
-	bus.arg = strtok(NULL, " \n\t"); /* Extract the argument of the operation */
-	while (opst[i].opcode && op)
+	if (*head == NULL)
 	{
-		if (strcmp(op, opst[i].opcode) == 0)
-		{
-			/* Call the corresponding function for the operation */
-			opst[i].f(stack, counter);
-			return (0);
-		}
-		i++;
-	}
-
-	/* Print an error message for unknown instruction, then clean up and exit */
-	if (op && opst[i].opcode == NULL)
-	{
-		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
-		fclose(file);
-		free(content);
-		free_stack(*stack);
+		fprintf(stderr, "L%u: can't pint, stack empty\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
 		exit(EXIT_FAILURE);
 	}
-	return (1);
+
+	printf("%d\n", (*head)->n);
 }
