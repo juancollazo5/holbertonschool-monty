1ff2169612ccc31ce995533b5d2ae55b4b469884
juancollazo5 <juancollazo577@outlook.com>
2024-01-08 06:45:06 +0000

    Documentation update
    add coments in the function

--- functions.c
+++ functions.c
@@ -17,23 +17,29 @@ int rpl(char *content, stack_t **stack, unsigned int counter, FILE *file)
 	unsigned int i = 0;
 	char *op;
 
-	op = strtok(content, " \n\t");
+	op = strtok(content, " \n\t"); /* Tokenize the content to extract the operation */
 	if (op && op[0] == '#')
-		return (0);
-	bus.arg = strtok(NULL, " \n\t");
+		return (0); /* Ignore comments (lines starting with '#') */
+	bus.arg = strtok(NULL, " \n\t"); /* Extract the argument of the operation */
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{	opst[i].f(stack, counter);
+		{
+			/* Call the corresponding function for the operation */
+			opst[i].f(stack, counter);
 			return (0);
 		}
 		i++;
 	}
+
+	/* Print an error message for unknown instruction, then clean up and exit */
 	if (op && opst[i].opcode == NULL)
-	{ fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
+	{
+		fprintf(stderr, "L%d: unknown instruction %s\n", counter, op);
 		fclose(file);
 		free(content);
 		free_stack(*stack);
-		exit(EXIT_FAILURE); }
+		exit(EXIT_FAILURE);
+	}
 	return (1);
 }
