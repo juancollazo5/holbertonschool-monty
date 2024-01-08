7a74e622cf111f53389d70621f30d4cda77703d0
juancollazo5 <juancollazo577@outlook.com>
2024-01-08 06:24:38 +0000

    Documentation update
    add coments in the function

--- node.c
+++ node.c
@@ -12,8 +12,8 @@ void addnode(stack_t **head, int n)
 {
 	stack_t *new_node, *temp;
 
-	temp = *head;
-	new_node = malloc(sizeof(stack_t));
+	temp = *head; /* Store the current head of the stack in a temporary variable */
+	new_node = malloc(sizeof(stack_t)); /* Allocate memory for a new node */
 
 	if (new_node == NULL)
 	{
@@ -21,10 +21,18 @@ void addnode(stack_t **head, int n)
 		exit(0);
 	}
 	if (temp)
+
+	/* If the stack is not empty, update the previous pointer of the current head */
 		temp->prev = new_node;
 
-	new_node->n = n;
+	new_node->n = n; /* Set the data value of the new node to the given integer */
+
+	/* Set the next pointer of the new node to the current head of the stack */
 	new_node->next = *head;
+	
+	/* Set the previous pointer of the new node to NULL, as it is the new head */
 	new_node->prev = NULL;
+
+	/* Update the head of the stack to the new node */
 	*head = new_node;
 }
