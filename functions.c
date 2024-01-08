ba4fb20fd9a8468e991c65ce2e24c8c9eab4d72a
juancollazo5 <juancollazo577@outlook.com>
2024-01-08 23:54:59 +0000

    new function
    add pop function

--- functions.c
+++ functions.c
@@ -21,4 +21,28 @@ void pint(stack_t **head, unsigned int counter)
 	printf("%d\n", (*head)->n);
 }
 
+/**
+ * pop - print top node on stack
+ * @head: pointer to pointer of first node
+ * @counter: line counter
+ */
+
+void pop(stack_t **head, unsigned int counter)
+{
+	stack_t *h;
+
+	if (*head == NULL)
+	{
+		fprintf(stderr, "L%d: can't pop an empty stack\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+
+	h = *head;
+	*head = h->next;
+	free(h);
+}
+
 /* others new functions on the next lines */
