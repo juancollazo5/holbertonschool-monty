4abe841a02f4e9c1c3793c5043e18611a393dc38
juancollazo5 <131397912+juancollazo5@users.noreply.github.com>
2024-01-09 23:59:03 -0400

    Update monty.h

--- monty.h
+++ monty.h
@@ -67,6 +67,5 @@ void pint(stack_t **head, unsigned int counter);
 void pop(stack_t **head, unsigned int counter);
 void swap(stack_t **head, unsigned int counter);
 void nop(stack_t **head, unsigned int counter);
-void add(stack_t **head, unsigned int counter);
 
 #endif
